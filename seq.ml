2d20e4884bb061196ac673ddd59ca92ea65c2bc0
Anton Yabchinskiy <arn@bestmx.net>
2016-10-12 13:42:53 +0300

    Seq: add {of,to}_string functions

--- seq.ml
+++ seq.ml
@@ -1,3 +1,5 @@
+open Batteries
+
 module type Elt_sig = sig
   type t
   val n : int
@@ -9,6 +11,16 @@ end
 
 module Make (Elt : Elt_sig) : sig
   type t
+
+  val of_string : string -> t
+
+  val to_string : t -> string
 end = struct
   type t = string
+
+
+  let of_string = String.map Elt.(to_char % of_char)
+
+
+  let to_string seq = seq
 end
