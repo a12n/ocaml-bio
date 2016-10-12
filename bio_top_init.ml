f5b96b33342eccabc724ac16e042cb8b6eba1ea7
Anton Yabchinskiy <arn@bestmx.net>
2016-10-13 01:08:03 +0300

    Add toplevel pp function for Rna.t

--- bio_top_init.ml
+++ bio_top_init.ml
@@ -4,5 +4,8 @@ let pp_dna = pp Bio.Dna.to_string;;
 
 let pp_dna_ambig = pp Bio.Dna.Ambig.to_string;;
 
+let pp_rna = pp Bio.Rna.to_string;;
+
 #install_printer pp_dna;;
 #install_printer pp_dna_ambig;;
+#install_printer pp_rna;;
