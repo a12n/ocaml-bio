90ff01d58fbba30985a4bed6466dbe53d2fac6ff
Anton Yabchinskiy <arn@bestmx.net>
2016-10-12 14:38:38 +0300

    Dna: add interface file

---
+++ dna.mli
@@ -0,0 +1,15 @@
+module Nt : sig
+  type t = A | C | G | T
+
+  val comp : t -> t
+  val n : int
+  val of_char : char -> t
+  val of_int : int -> t
+  val to_char : t -> char
+  val to_int : t -> int
+end
+
+include module type of Seq.Make (Nt)
+
+val comp : t -> t
+val rev_comp : t -> t
