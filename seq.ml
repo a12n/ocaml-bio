2ac11fce617db9d5d85a7596b2e8850a93859b4b
Anton Yabchinskiy <arn@bestmx.net>
2016-10-12 13:44:56 +0300

    Seq: add {of,to}_array functions

--- seq.ml
+++ seq.ml
@@ -12,16 +12,20 @@ end
 module Make (Elt : Elt_sig) : sig
   type t
 
+  val of_array : Elt.t array -> t
   val of_enum : Elt.t Enum.t -> t
   val of_list : Elt.t list -> t
   val of_string : string -> t
 
   val enum : t -> Elt.t Enum.t
+  val to_array : t -> Elt.t array
   val to_list : t -> Elt.t list
   val to_string : t -> string
 end = struct
   type t = string
 
+
+  let of_array arr = String.init (Array.length arr) (Elt.to_char % Array.get arr)
 
   let of_enum enum = String.of_enum (Enum.map Elt.to_char enum)
 
@@ -31,6 +35,8 @@ end = struct
 
 
   let enum = Enum.map Elt.of_char % String.enum
+
+  let to_array = Array.of_enum % enum
 
   let to_list = List.map Elt.of_char % String.to_list
 
