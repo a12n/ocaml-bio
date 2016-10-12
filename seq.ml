2401a7e6da246a2b20434197f35de48218b65da0
Anton Yabchinskiy <arn@bestmx.net>
2016-10-12 13:44:12 +0300

    Seq: add of_enum and enum functions

--- seq.ml
+++ seq.ml
@@ -12,19 +12,25 @@ end
 module Make (Elt : Elt_sig) : sig
   type t
 
+  val of_enum : Elt.t Enum.t -> t
   val of_list : Elt.t list -> t
   val of_string : string -> t
 
+  val enum : t -> Elt.t Enum.t
   val to_list : t -> Elt.t list
   val to_string : t -> string
 end = struct
   type t = string
 
 
+  let of_enum enum = String.of_enum (Enum.map Elt.to_char enum)
+
   let of_list list = String.of_list (List.map Elt.to_char list)
 
   let of_string = String.map Elt.(to_char % of_char)
 
+
+  let enum = Enum.map Elt.of_char % String.enum
 
   let to_list = List.map Elt.of_char % String.to_list
 
