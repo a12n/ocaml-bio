67eca1e89a396124f23346bb41db1f9373f304f1
Anton Yabchinskiy <arn@bestmx.net>
2016-10-12 15:55:00 +0300

    Add toplevel pretty-printer for ambiguous DNA

--- bio_top_init.ml
+++ bio_top_init.ml
@@ -1,3 +1,6 @@
 let pp_dna f dna = Format.fprintf f "\"%s\"" (Bio.Dna.to_string dna);;
 
+let pp_dna_ambig f dna = Format.fprintf f "\"%s\"" (Bio.Dna.Ambig.to_string dna);;
+
 #install_printer pp_dna;;
+#install_printer pp_dna_ambig;;
