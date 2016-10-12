b63b9721ff4af53bac9be153e51d1592497aea06
Anton Yabchinskiy <arn@bestmx.net>
2016-10-12 13:43:34 +0300

    Seq: add {of,to}_list functions

--- seq.ml
+++ seq.ml
@@ -12,15 +12,21 @@ end
 module Make (Elt : Elt_sig) : sig
   type t
 
+  val of_list : Elt.t list -> t
   val of_string : string -> t
 
+  val to_list : t -> Elt.t list
   val to_string : t -> string
 end = struct
   type t = string
 
 
+  let of_list list = String.of_list (List.map Elt.to_char list)
+
   let of_string = String.map Elt.(to_char % of_char)
 
+
+  let to_list = List.map Elt.of_char % String.to_list
 
   let to_string seq = seq
 end
