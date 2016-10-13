5afa1929a9aee917c40e268a70d68522125c12e1
Anton Yabchinskiy <arn@bestmx.net>
2016-10-13 12:05:41 +0300

    Aa: functions for amino acid abbreviation and name

--- aa.mli
+++ aa.mli
@@ -1,3 +1,6 @@
 type aa = A | R | N | D | C | Q | E | G | H | I | L
         | K | M | F | P | O | S | U | T | W | Y | V
 include Seq.Elt_sig with type t = aa
+
+val abbr : t -> string
+val name : t -> string
