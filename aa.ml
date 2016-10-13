17173a0008458681a64d263d83460ec2894b659b
Anton Yabchinskiy <arn@bestmx.net>
2016-10-13 13:17:34 +0300

    Aa: function for monoisotopic mass

--- aa.ml
+++ aa.ml
@@ -126,6 +126,32 @@ let abbr = function
   | Y -> "Tyr"
   | V -> "Val"
 
+(** Monoisotopic mass. The sum of the masses of the most common
+    isotopes of the atoms making up a molecule. *)
+let mass = function
+  | A ->  71.03711
+  | R -> 156.10111
+  | N -> 114.04293
+  | D -> 115.02694
+  | C -> 103.00919
+  | Q -> 128.05858
+  | E -> 129.04259
+  | G ->  57.02146
+  | H -> 137.05891
+  | I -> 113.08406
+  | L -> 113.08406
+  | K -> 128.09496
+  | M -> 131.04049
+  | F -> 147.06841
+  | P ->  97.05276
+  | O -> 237.14773
+  | S ->  87.03203
+  | U -> 150.95363
+  | T -> 101.04768
+  | W -> 186.07931
+  | Y -> 163.06333
+  | V ->  99.06841
+
 let name = function
   | A -> "Alanine"
   | R -> "Arginine"
--- aa.mli
+++ aa.mli
@@ -3,4 +3,5 @@ type aa = A | R | N | D | C | Q | E | G | H | I | L
 include Seq.Elt_sig with type t = aa
 
 val abbr : t -> string
+val mass : t -> float
 val name : t -> string
