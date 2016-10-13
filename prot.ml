61a0039e99f423ffe7af755f7bbaf8fc32522b68
Anton Yabchinskiy <arn@bestmx.net>
2016-10-13 16:10:12 +0300

    Prot: more tests for edit_dist

--- prot.ml
+++ prot.ml
@@ -2,6 +2,13 @@ open BatPervasives
 
 include Seq.Make (Aa)
 
+(*$= edit_dist
+  (edit_dist (of_string "") (of_string "MMM")) 3
+  (edit_dist (of_string "MMM") (of_string "")) 3
+  (edit_dist (of_string "MMM") (of_string "MMM")) 0
+  (edit_dist (of_string "PLEASANTLY") (of_string "MEANLY")) 5
+*)
+
 let mass_aux f = fold_left (fun ans aa -> ans +. f aa) 0.0
 
 let avg_mass = mass_aux Aa.avg_mass
