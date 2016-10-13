129c95fc61be01a8c716e293f021b0cf74bacce4
Anton Yabchinskiy <arn@bestmx.net>
2016-10-13 13:31:50 +0300

    Add toplevel pp for proteins

--- bio_top_init.ml
+++ bio_top_init.ml
@@ -4,8 +4,11 @@ let pp_dna = pp Bio.Dna.to_string;;
 
 let pp_dna_ambig = pp Bio.Dna.Ambig.to_string;;
 
+let pp_prot = pp Bio.Prot.to_string;;
+
 let pp_rna = pp Bio.Rna.to_string;;
 
 #install_printer pp_dna;;
 #install_printer pp_dna_ambig;;
+#install_printer pp_prot;;
 #install_printer pp_rna;;
