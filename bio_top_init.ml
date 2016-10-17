3752b721c282eaa2193bfea5e871c4766889bfea
Anton Yabchinskiy <arn@bestmx.net>
2016-10-17 23:20:59 +0300

    bio_top_init.ml: use Seq.print to define toplevel pp functions

--- bio_top_init.ml
+++ bio_top_init.ml
@@ -1,12 +1,9 @@
-let pp f fmt s = Format.fprintf fmt "\"%s\"" (f s);;
+open Batteries
 
-let pp_dna = pp Bio.Dna.to_string;;
-
-let pp_dna_ambig = pp Bio.Dna.Ambig.to_string;;
-
-let pp_prot = pp Bio.Prot.to_string;;
-
-let pp_rna = pp Bio.Rna.to_string;;
+let pp_dna = IO.to_f_printer Bio.Dna.print;;
+let pp_dna_ambig = IO.to_f_printer Bio.Dna.Ambig.print;;
+let pp_prot = IO.to_f_printer Bio.Prot.print;;
+let pp_rna = IO.to_f_printer Bio.Rna.print;;
 
 #install_printer pp_dna;;
 #install_printer pp_dna_ambig;;
