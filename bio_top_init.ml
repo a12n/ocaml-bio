34f270a73524e750a7b57c2e08c4362f6b09d21b
Anton Yabchinskiy <arn@bestmx.net>
2016-10-17 23:30:28 +0300

    bio_top_init.ml: load cma file, open Bio

--- bio_top_init.ml
+++ bio_top_init.ml
@@ -1,9 +1,15 @@
 open Batteries
 
-let pp_dna = IO.to_f_printer Bio.Dna.print;;
-let pp_dna_ambig = IO.to_f_printer Bio.Dna.Ambig.print;;
-let pp_prot = IO.to_f_printer Bio.Prot.print;;
-let pp_rna = IO.to_f_printer Bio.Rna.print;;
+open Batteries;;
+
+#load "_build/bio.cma";;
+
+open Bio;;
+
+let pp_dna = IO.to_f_printer Dna.print;;
+let pp_dna_ambig = IO.to_f_printer Dna.Ambig.print;;
+let pp_prot = IO.to_f_printer Prot.print;;
+let pp_rna = IO.to_f_printer Rna.print;;
 
 #install_printer pp_dna;;
 #install_printer pp_dna_ambig;;
